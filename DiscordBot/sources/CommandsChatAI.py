cbcb03cc118086716d64fa73b107f85f8ef8bafa
name5go <107245702+name5go@users.noreply.github.com>
2023-04-06 00:56:02 +0900

    ChatGPTとの対話機能を実装

--- DiscordBot/sources/CommandsChatAI.py
+++ DiscordBot/sources/CommandsChatAI.py
@@ -1,7 +1,30 @@
 # -*- coding: Shift-JIS -*-
 
 from discord.ext import commands
+from discord.commands import Option
+import openai
+
+#環境変数取得用
+import os
 
 class ChatAI(commands.Cog):
  def __init__(self, bot):
   self.bot = bot
+  self.openai.api_key=os.environ.get('OPENAI_TOKEN')
+  self.messages_history=[{
+                   "role": "system",
+                   "content": "「ずんだもん」という名前の可愛い女の子のキャラクターがいる。そのキャラは語尾が「なのだ」、一人称は「ぼく」が特徴のキャラクターだ。自己紹介は不要です。この文以降の返信をしてほしい、だが前述のキャラクターになりきって返信して。"
+                    },],
+ @commands.slash_command(name='a_aiと会話', description='AIと会話。限界はわからん')
+ async def talk_ai(self,ctx,
+                   message:Option(str, 'メッセージ')
+                   ):
+  self.messages_history.apeend({"role":"user","content":message})
+  response = openai.ChatCompletion.create(
+                                           model="gpt-3.5-turbo",
+                                           messsages=self.messages_history,
+                                           )
+  #await ctx.respond(message+'サーバー名します')
+  reply=str(response[0]["message"]["content"].strip())
+  self.messages_history.apeend({"role":"assistant","content":reply})
+  await ctx.respond(reply)
